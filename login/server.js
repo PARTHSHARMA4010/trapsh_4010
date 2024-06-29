3e3cf8d4182ea8abe4222505b4765125adeaee5f
PARTHSHARMA4010 <2023kucp1020@iiitkota.ac.in>
2024-06-29 17:03:35 +0530

    working on backend but facing many problems....

--- login/server.js
+++ login/server.js
@@ -1,56 +1,39 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoutes from './src/routes/auth.js';
+import { DB_NAME } from './src/constants';
 
-// const app = express();
-// const port = process.env.PORT || 5000;
+dotenv.config();
 
-// // Middleware
-// app.use(cors());
-// app.use(bodyParser.json());
+const app = express();
 
-// // MongoDB connection
-// mongoose.connect('mongodb+srv://parthsharma4010:parth123@cluster0.fnhxi59.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', () => {
-//   console.log('Connected to MongoDB');
-// });
+// Routes
+app.use('/api/auth', authRoutes);
 
-// // User Schema
-// const userSchema = new mongoose.Schema({
-//   username: String,
-//   password: String,
-//   mobileNumber: String,
-//   otpVerified: Boolean,
-// });
+;(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
 
-// const User = mongoose.model('User', userSchema);
+    app.on('error', (error) => {
+      console.log('Error : ', error);
+      throw error;
+    });
 
-// // Routes
-// app.post('/register', async (req, res) => {
-//   const { username, password, mobileNumber, otpVerified } = req.body;
-
-//   const newUser = new User({
-//     username,
-//     password,
-//     mobileNumber,
-//     otpVerified,
-//   });
-
-//   try {
-//     const savedUser = await newUser.save();
-//     res.status(201).json(savedUser);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error('ERROR :', error);
+    throw error;
+  }
+})();
